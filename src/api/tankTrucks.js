ade3946be3bbd791bf852baaf7f8484932eac206
Ortiz <dieortiz@amazon.com>
2017-10-01 20:34:16 -0700

    Load tank trucks from graphql endpoint

--- src/api/tankTrucks.js
+++ src/api/tankTrucks.js
@@ -1,1 +1,28 @@
-import graphql from './graphql';
+// @flow
+import gql from 'graphql-tag';
+
+import grapqhl from './graphql';
+
+export const getTankTrucks = (): Promise<*> => {
+    const query = gql`query {
+        viewer {
+            allTankTrucks {
+                edges {
+                    node {
+                        id
+                        location {
+                            lat
+                            lng
+                        }
+                    }
+                }
+            }
+        }
+    }`;
+
+    const response = grapqhl(query);
+
+    return response
+        .then(response => response.data.viewer.allTankTrucks.edges)
+        .then(response => response.map(edge => edge.node));
+};
--- src/sagas/tankTrucks.js
+++ src/sagas/tankTrucks.js
@@ -2,12 +2,15 @@ import { call, delay, takeEvery, throttle } from 'redux-saga';
 import { put } from 'redux-saga/effects';
 
 import { updateTankTrucks } from '../actions/tankTrucks';
-// import { subscribe, unsubscribe } from '../api/tankTrucks';
+import { getTankTrucks } from '../api/tankTrucks';
 
 let subscription = null;
 
 function* subscribeWorker() {
     console.log('Subcribed');
+    console.warn('Subscription implementation is missing!');
+    const tankTrucks = yield getTankTrucks();
+    yield put(updateTankTrucks(tankTrucks));
 }
 
 function* unsubscribeWorker() {
